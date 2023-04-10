0dc27147cdedb54d3a5232e8ebe42d953bd654ae
AyushManglik <91412342+AyushManglik@users.noreply.github.com>
2023-04-10 08:51:33 +0530

    Day-3

---
+++ Study_Plan/LeetCode_75/21_Merge_Two_Sorted_Lists.java
@@ -0,0 +1,43 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode mergeTwoLists(ListNode a, ListNode b) {
+        ListNode head = null,tail = null;
+        if(a==null) 
+            return b;
+        else if(b==null) 
+            return a;
+        else if(a.val<=b.val) {
+            head = tail = a;   
+            a = a.next;
+        }
+        else{ 
+            head = tail = b;   
+            b = b.next;
+        }
+
+        while(a!=null && b!=null){
+            if(a.val<=b.val){
+                tail.next = a;   
+                tail = a;   
+                a = a.next; }
+            else{
+                tail.next = b;   
+                tail = b;   
+                b = b.next;  } 
+        }
+        if(a==null) 
+            tail.next = b;
+        else 
+            tail.next = a;
+        return head;
+    }
+}
