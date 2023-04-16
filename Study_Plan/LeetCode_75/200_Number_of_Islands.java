b603153bc5c43cdcbe2823a4f33359608594c75c
AyushManglik <91412342+AyushManglik@users.noreply.github.com>
2023-04-16 23:17:32 +0530

    Day-9_b

---
+++ Study_Plan/LeetCode_75/200_Number_of_Islands.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int numIslands(char[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
+        boolean[][] visited = new boolean[m][n];
+        int numOfIslands = 0;
+        for(int i=0; i<m;i++){
+            for(int j=0;j<n;j++){
+                if(!visited[i][j] && grid[i][j] == '1'){
+                    dfs(grid, i,j, visited);
+                    numOfIslands++;
+                }
+            }
+        }
+        return numOfIslands;
+    }
+    void dfs(char[][] grid, int row, int col, boolean[][] visited){
+        if((row<0 || col<0) || row>=grid.length || col>=grid[0].length || visited[row][col] || grid[row][col] == '0'){
+            return;
+        }
+        visited[row][col] = true;
+        dfs(grid, row, col-1, visited); //go left
+        dfs(grid, row-1, col, visited); //go up
+        dfs(grid, row, col+1, visited); //go right
+        dfs(grid, row+1, col, visited); //go down
+
+    }
+}
