00d8d7b8ba6c8aa494da987ac606958f0f0d1ce3
AyushManglik <91412342+AyushManglik@users.noreply.github.com>
2023-05-02 09:41:54 +0530

    1822. Sign of the Product of an Array

---
+++ Daily Challenges/MAY_2023/02_MAY_2023.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int arraySign(int[] nums) {
+        int count = 0;
+        for (int i=0; i<nums.length; i++){
+            if (nums[i] < 0){
+                count++;
+            }
+            else if (nums[i] == 0){
+                return 0;
+            }
+        }
+        if (count%2 == 0){
+            return 1;
+        }
+        return -1;
+    }
+}
