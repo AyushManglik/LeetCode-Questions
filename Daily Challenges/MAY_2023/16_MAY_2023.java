2ce6b48700906d6991c69cb8ef0f5d55955b93e1
AyushManglik <91412342+AyushManglik@users.noreply.github.com>
2023-05-16 10:00:40 +0530

    24. Swap Nodes in Pairs

---
+++ Daily Challenges/MAY_2023/16_MAY_2023.java
@@ -0,0 +1,22 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode swapPairs(ListNode head) {
+        if (head == null || head.next == null) return head;
+        ListNode p1 = head;
+        ListNode p2 = head.next;
+        ListNode p3 = p2.next;
+        p1.next = p3;
+        p2.next = p1;
+        if(p3 != null) p1.next = swapPairs(p3);
+        return p2;
+    }
+}
