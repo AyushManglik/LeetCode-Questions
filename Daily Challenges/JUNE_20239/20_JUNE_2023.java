8b7a33e0a83f80ab9e92438307c48c942fdd6175
AyushManglik <91412342+AyushManglik@users.noreply.github.com>
2023-06-20 18:31:10 +0530

    2090. K Radius Subarray Averages

---
+++ Daily Challenges/JUNE_20239/20_JUNE_2023.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int[] getAverages(int[] nums, int k) {
+
+        int[] avgs = new int[nums.length];
+
+        Arrays.fill(avgs, -1);
+
+        if(2 * k + 1 > nums.length){
+            return avgs;
+        }
+
+        double sum = 0;
+
+        for(int i = 0; i < 2 * k + 1; i++){
+            sum += nums[i];
+        }
+
+        double avg = sum / (2 * k + 1);
+        avgs[k] = (int)avg;
+
+        for(int i = 2 * k + 1, c = 0; i < nums.length; i++, c++){
+            sum += nums[i];
+            sum -= nums[c];
+
+            avg = sum / (2 * k + 1);
+            avgs[k + c + 1] = (int)avg;
+
+        }
+
+        return avgs;
+
+    }
+}
