71510a2f694c9ded073e7c84a5e6725edb1a0348
AyushManglik <91412342+AyushManglik@users.noreply.github.com>
2023-07-07 10:15:42 +0530

    Delete Study_Plan/LeetCode_75 directory

--- Study_Plan/LeetCode_75/589_N-ary_Tree_Preorder_Traversal.java
+++
@@ -1,9 +0,0 @@
-class Solution {
-    List<Integer> list = new ArrayList<>();
-    public List<Integer> preorder(Node root) {
-        if(root==null)return list;
-        list.add(root.val);
-        for(Node i: root.children) preorder(i);
-        return list;
-    }
-}
