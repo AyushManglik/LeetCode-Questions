f163cadf1e88d0914d7a66de5e79e57334b5275c
AyushManglik <91412342+AyushManglik@users.noreply.github.com>
2023-07-25 11:35:34 +0530

    852. Peak Index in a Mountain Array

---
+++ Daily Challenges/JULY2023/25_JULY_2023.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int peakIndexInMountainArray(int[] A) {
+        int i = 0;
+        while (A[i] < A[i+1]) i++;
+        return i;
+    }
+}
+
+// Same as Problem 161. Find Peak Element
