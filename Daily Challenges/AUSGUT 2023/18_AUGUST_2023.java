ec7c4d16561d85517d698a16764fe6e720db7851
AyushManglik <91412342+AyushManglik@users.noreply.github.com>
2023-08-18 10:44:06 +0530

    1615. Maximal Network Rank

---
+++ Daily Challenges/AUSGUT 2023/18_AUGUST_2023.java
@@ -0,0 +1,105 @@
+// 100%
+/*
+class Solution {
+    public int maximalNetworkRank(int n, int[][] roads) {
+        // calculate all node degrees
+        int[] degrees = new int[n];
+        for (int[] road : roads) {
+            degrees[road[0]]++;
+            degrees[road[1]]++;
+        }
+        
+        // find first max and second max degree
+        int firstMax = 0, secondMax = 0;
+        for (int degree : degrees) {
+            if (degree > firstMax) {
+                secondMax = firstMax;
+                firstMax =  degree;
+            }
+            else if (degree > secondMax && degree < firstMax) {
+                secondMax = degree;
+            }
+        }
+        
+        // there can be multiple node with firstMax degree or secondMax degree
+        // find the count of such nodes
+        int fmCount = 0, smCount = 0;
+        for (int degree : degrees) {
+            if (degree == firstMax) fmCount++;
+            if (degree == secondMax) smCount++;
+        }
+        
+        // if there are more than one node with degree = 'firstMax'
+        // then we can consider 'firstMax' twice and we don't need to use 'secondMax'
+        if (fmCount > 1) {
+            // here the answer in general will be 2*firstMax
+            // but if the two nodes that we're considering are connected, then we'll have to subtract 1
+            // in other words, if there are atleast one such pair, whose degree = 'firstMax' and
+            // they're not connected then answer is 2*firstMax, else if all nodes with degree = 'firstMax'
+            // are connected to each other, then answer is 2*firstMax-1, as we'll have one common edge
+            int edgeCount = 0;
+            for (int[] road : roads)
+                if (degrees[road[0]] == firstMax && degrees[road[1]] == firstMax) edgeCount++;
+            
+            // maximum number of edges possible with all the nodes with degree = 'firstMax'
+            int maxEdgeWithFirstMax = fmCount * (fmCount-1) / 2;
+            
+            // if all nodes with degree = 'firstMax' are connected to each other, then subtract 1
+            return 2 * firstMax - (maxEdgeWithFirstMax == edgeCount ? 1 : 0);
+        }
+        
+        // if there is only one node with degree = 'firstMax'
+        // then we'll need to use node with degree = 'secondMax'
+        else {
+            // here the answer in general will be (firstMax + secondMax)
+            // but if the two nodes that we're considering are connected, then we'll have to subtract 1
+            // in other words, if there are atleast one such pair, whose degree = 'firstMax' and 'secondMax'
+            // and they're not connected then answer is (firstMax + secondMax), else if all pairs
+            // are connected to each other, then answer is (firstMax + secondMax - 1), as we'll have one common edge
+            int edgeCount = 0;
+            for (int[] road : roads) {
+                if (degrees[road[0]] == firstMax && degrees[road[1]] == secondMax) edgeCount++;
+                if (degrees[road[0]] == secondMax && degrees[road[1]] == firstMax) edgeCount++;
+            }
+            
+            // if all pairs are connected to each other, then subtract 1
+            return firstMax + secondMax - (smCount == edgeCount ? 1 : 0);
+        }
+    }
+}
+
+*/
+
+// 97%
+class Solution {
+    public int maximalNetworkRank(int n, int[][] roads) {
+        int[][] graph = new int[n][n];
+        int[] degree = new int[n];
+
+        for(int[] info : roads){
+            int u = info[0];
+            int v = info[1];
+
+            graph[u][v] = 1;
+            graph[v][u] = 1;
+
+            degree[u]++;
+            degree[v]++;
+        }
+
+        int ans = 0;
+
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                int rank = degree[i] + degree[j];
+                if(rank<ans) continue;
+
+                if(graph[i][j]==1) rank--;
+
+                ans = Math.max(ans,rank);
+            }
+        }
+
+        return ans;
+    }
+}
