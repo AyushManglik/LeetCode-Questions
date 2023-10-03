da20bf3abc615217c0b42e6a8905b0d8fbeeabfa
AyushManglik <91412342+AyushManglik@users.noreply.github.com>
2023-10-03 18:36:58 +0530

    1512. Number of Good Pairs

---
+++ Daily Challenges/OCTOBER 2023/03_10_2023.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int numIdenticalPairs(int[] nums) {
+        int count = 0;
+        for (int i=0;i<nums.length-1;i++){
+            for(int j=0;j<nums.length;j++){
+                if(i==j){
+                    continue;
+                }
+                if(nums[i] == nums[j] && i<j){
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+}
